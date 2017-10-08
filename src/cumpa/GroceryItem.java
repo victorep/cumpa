d3c9f9c93ad4d03d2db4a540b591b8524f125b83
victor <victor.epure@gmail.com>
2017-10-08 08:58:48 +0300

    added the properties and generated two contrusctors and setters/getters

--- src/cumpa/GroceryItem.java
+++ src/cumpa/GroceryItem.java
@@ -1,5 +1,32 @@
 package cumpa;
 
 public class GroceryItem {
+    private  String itemName;
+    private  int itemUnits;
 
+
+
+    public GroceryItem() {
+    }
+
+    public GroceryItem(String itemName, int itemUnits) {
+        this.itemName = itemName;
+        this.itemUnits = itemUnits;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemUnits() {
+        return itemUnits;
+    }
+
+    public void setItemUnits(int itemUnits) {
+        this.itemUnits = itemUnits;
+    }
 }

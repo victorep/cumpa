6994a9c511e923e17be891fd2d70f0ba3d3a0cc3
victor <victor.epure@gmail.com>
2017-10-08 07:27:59 +0300

    defined and showing one item properties

--- src/cumpa/App.java
+++ src/cumpa/App.java
@@ -5,12 +5,23 @@ public class App {
     public static void main(String[] args) {
         String appName = "Our Groceries";
         double appVersion = 0.1;
-        
-        //System.out.println("This will be an app called " + appName + ". It will help families organise groceries");
+
+        //our grocery item
+        String itemName = "Milk 1L";
+        String itemCategory = "Food";
+        double itemUnitPrice = 5.6;
+        int itemUnits = 2;
+        double itemTotalPrice = itemUnits*itemUnitPrice;
 
         System.out.println("Initializing " + appName + " version " + appVersion);
 
-
+        System.out.println("=== Our product ===");
+        System.out.printf("Name: %s \n",itemName);
+        System.out.printf("category: %s \n",itemCategory);
+        System.out.printf("Unit proice: %s \n",itemUnitPrice);
+        System.out.printf("Total units: %s \n",itemUnits);
+        System.out.printf("Total price: %s \n",itemTotalPrice);
+        System.out.println("===================");
 
         System.out.println("Application finished");
 

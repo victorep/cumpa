158518636cb07058f7baa9ca0c5adacabd4f2d90
victor <victor.epure@gmail.com>
2017-10-08 07:11:18 +0300

    Changed the appVersion to double to allow minor versions

--- src/cumpa/App.java
+++ src/cumpa/App.java
@@ -4,10 +4,13 @@ public class App {
 
     public static void main(String[] args) {
         String appName = "Our Groceries";
-        int appVersion = 1;
+        double appVersion = 0.1;
+        
         //System.out.println("This will be an app called " + appName + ". It will help families organise groceries");
 
         System.out.println("Initializing " + appName + " version " + appVersion);
+
+
 
         System.out.println("Application finished");
 

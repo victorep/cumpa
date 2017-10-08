9a8ce7d6660e9bbae30cdd5842d51b78f72a4552
victor <victor.epure@gmail.com>
2017-10-08 07:01:10 +0300

    Initial commit of chapter 1

--- src/cumpa/App.java
+++ src/cumpa/App.java
@@ -1,6 +1,13 @@
 package cumpa;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class App {
 
-    public static void main(String[] args) {    }
+    public static void main(String[] args) {
+
+        System.out.println("This will be an app called Cumpa. It will help families organise groceries");
+
+    }
 
 }
